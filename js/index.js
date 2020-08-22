cb6f7b777661d2e5e53eeed506fedb0e1ceb058d
Adrian Figueroa Rodrigue <adrianfigueroarodriguez@outlook.com>
2020-08-22 14:21:36 -0600

    Dia 2: 1/2

--- js/index.js
+++ js/index.js
@@ -6,6 +6,9 @@ setTimeout(function(){
     $("#initialScreen").hide();
     $(".initialScreen").css("background", "rgba(26, 100, 209, 1)");
     $("nav").css("border-bottom", "#ffffff solid 1px");
+    if((window.matchMedia("(min-width: 768px)").matches)){
+        $('.nav-options').css('visibility', 'visible');
+    } 
     //$("nav").css("background-color", "rgba(64, 159, 239, 1)");
     $("#main-screen-section").show();
     $("#main-screen-section").css("padding-top", "26px");
@@ -24,4 +27,23 @@ function hide(idOfObject){
 
 function show(idOfObject){
     $(`#` + `${idOfObject}`).show();
+}
+
+function nosotrosSomosImgExpand(){
+    /* console.log($(`#nosotros-somos-section`).prop('clientHeight')); */
+    if($(`#nosotros-somos-section`).prop('clientHeight') == 229){
+        $(`#nosotros-somos-section`).css("display", "block");
+        $(`.col-lg-6`).css("padding", "0px");
+        document.getElementById(`nosotros-somos-img`).classList.remove('col-lg-6');
+        document.getElementById(`nosotros-somos-img`).classList.add('col-lg-12');
+        document.getElementById(`nosotros-somos-text`).classList.remove('col-lg-6');
+        document.getElementById(`nosotros-somos-text`).classList.add('col-lg-12');
+    } else{
+        $(`#nosotros-somos-section`).css("display", "inline-flex");
+        $(`#nosotros-somos-text`).css("padding-left", "30px");
+        document.getElementById(`nosotros-somos-img`).classList.remove('col-lg-12');
+        document.getElementById(`nosotros-somos-img`).classList.add('col-lg-6');
+        document.getElementById(`nosotros-somos-text`).classList.remove('col-lg-12');
+        document.getElementById(`nosotros-somos-text`).classList.add('col-lg-6');
+    }
 }
